fa4e52ef0aff7f7bdea09cd1496ba721a675b84d
Diogenes Yazan <76189529+DiogenesYazan@users.noreply.github.com>
2022-08-02 23:06:59 -0300

    Add files via upload

---
+++ App.js
@@ -0,0 +1,22 @@
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Esse é o primeiro app em react-native</Text>
+      <Text>Hello world</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
